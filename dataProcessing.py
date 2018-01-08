cc86e3a476de761a8471a5a9cccc8f3ff210d067
Pauly91 <abelpaul91@gmail.com>
2018-01-08 19:35:29 +0530

    dailyPlayerStats data fetched

--- dataProcessing.py
+++ dataProcessing.py
@@ -1,17 +1,31 @@
 import pandas as pd
+from ohmysportsfeedspy import MySportsFeeds
+import sys
 
 def buildScheduleList(pathToSchedule):       
     raw_df = pd.read_csv(pathToSchedule)
     df = pd.DataFrame()
-    df = pd.DataFrame([(row[0].split()[1]).replace('-','').replace(',','') for row in raw_df.values],columns=["Game Date"])
-    print(df.shape)
+    df = pd.DataFrame([(row[0].split()[1]).replace('-','').replace(',','').replace("'",'') for row in raw_df.values],columns=["Game Date"])
     df.drop_duplicates(inplace=True)
-    print(df.shape)
     return df
  
-def main():
-    schedule = 'results/full_game_schedule-nba-2016-2017-regular.csv'
-    df = buildScheduleList(schedule)
+def getDailyPlayerStatsData(df):
+    Data_query = MySportsFeeds('1.0',verbose=True)
+    Data_query.authenticate('poly91', 'abel1234')
+    for gameDate in df['Game Date'].tolist():
+        print(gameDate)
+        try:
+            Output = Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='daily_player_stats',fordate=str(gameDate),format='csv',sort='player.team')
+        except:
+            print("errorinfo: ", sys.exc_info()[0])
+            print('Issue with %d',gameDate)
 
+def main():
+    #schedule = 'results/full_game_schedule-nba-2016-2017-regular.csv'
+    #df = buildScheduleList(schedule)
+    #df.to_csv('nba_schedule_regular_20162017')
+    df = pd.read_csv('nba_schedule_regular_20162017')
+    print(df['Game Date'].tolist())
+    getDailyPlayerStatsData(df)
 if __name__ == '__main__':
     main()
