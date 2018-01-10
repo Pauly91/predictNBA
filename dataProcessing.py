7f7d3e414eb3cedb4946f223e461a75ddd10c003
Pauly91 <abelpaul91@gmail.com>
2018-01-10 08:44:56 +0530

    More edits

--- dataProcessing.py
+++ dataProcessing.py
@@ -16,11 +16,16 @@ N/W
 - Do all the above and compare performance.
 '''
 
-def buildScheduleList(pathToSchedule):       
-    raw_df = pd.read_csv(pathToSchedule)
+def getInitialData(pathToData):       
+    raw_df = pd.read_csv(pathToData)
+    # ["['#Date/Time of Update: 2018-01-05 9:10:38 AM' - 0, '#Game Date'- 1, '#Game Time'- 2, '#Away Team ID'- 3, 
+    # '#Away Team Abbr.'- 4, '#Away Team City'- 5, '#Away Team Name'- 6, '#Home Team ID'- 7, 
+    # '#Home Team Abbr.'- 8, '#Home Team City'- 9, '#Home Team Name'- 10, '#Location- 11']"]
     df = pd.DataFrame()
-    df = pd.DataFrame([(row[0].split()[1]).replace('-','').replace(',','').replace("'",'') for row in raw_df.values],columns=["Game Date"])
-    df.drop_duplicates(inplace=True)
+    print(raw_df.columns.tolist())
+    #df = pd.DataFrame([(row[0].split()[1]).replace('-','').replace(',','').replace("'",'') for row in raw_df.values],columns=["Game Date"])
+    #df.drop_duplicates(inplace=True)
+    #df.to_csv('nba_schedule_regular_20162017.csv')
     return df
  
 def getDailyPlayerStatsData(df):
@@ -34,13 +39,34 @@ def getDailyPlayerStatsData(df):
             print("errorinfo: ", sys.exc_info()[0])
             print('Issue with %d',gameDate)
 
+
+def buildNBAData():
+    '''
+    Algorithm:
+    - Iterate through files
+    - Find how to work through one csv
+    '''
+    df = pd.read_csv('results/daily_player_stats-nba-2016-2017-regular-20161026.csv')
+    columnNames = df.columns.tolist()[0].replace('[','').replace(']','').replace("'",'').replace(' ','').split(',')
+    print(columnNames)
+    
+    # '#Date/TimeofUpdate:2018-01-088:54:52AM' - 0, '#GameDate' - 1, '#GameTeams' - 2, '#PlayerID' - 3, 
+    # '#LastName' - 4, '#FirstName' - 5, '#Position' - 6, '#TeamID' - 7, '#TeamAbbr.' - 8, '#TeamCity' - 9, 
+    # '#TeamName - 10'
+    # 12:end is feature
+
 def main():
     #schedule = 'results/full_game_schedule-nba-2016-2017-regular.csv'
     #df = buildScheduleList(schedule)
-    #df.to_csv('nba_schedule_regular_20162017')
-    df = pd.read_csv('nba_schedule_regular_20162017')
-    print(df['Game Date'].tolist())
-    getDailyPlayerStatsData(df)
+    
+
+    #df = pd.read_csv('nba_schedule_regular_20162017')
+    #print(df['Game Date'].tolist())
+    #getDailyPlayerStatsData(df)
+    data = 'results/full_game_schedule-nba-2016-2017-regular.csv'
+    #buildNBAData()
+    getInitialData(data)
+
 if __name__ == '__main__':
     main()
 
--- getDataNBA.py
+++ getDataNBA.py
@@ -2,7 +2,7 @@ from ohmysportsfeedspy import MySportsFeeds
 
 Data_query = MySportsFeeds('1.0',verbose=True)
 Data_query.authenticate('poly91', 'abel1234')
-Output = Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='daily_player_stats',fordate='20161202',format='csv',sort='player.team')
+Output = Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='daily_player_stats',fordate='20161202',format='json',sort='player.team')
 #Output = Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='full_game_schedule',format='csv')
 #Output = Data_query.msf_get_data(league='nba',season='2016-2017-regular',feed='game_boxscore',format='csv')
 # 20161025
